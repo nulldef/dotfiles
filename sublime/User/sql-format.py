24880e2cc5bf80266d8ae1f2db3d9a575da06e98
Aleksei Bespalov <aleksei.bespalov@smartly.io>
2024-12-24 13:18:01 +0100

    use pgformatter for sql format

--- sublime/User/sql-format.py
+++ sublime/User/sql-format.py
@@ -8,10 +8,21 @@ class SqlFormatCommand(sublime_plugin.TextCommand):
     region = sublime.Region(0, file_size)
     content = self.view.substr(region)
 
-    command = ["sqlfmt"]
-    command.extend(["--print-width", "100"])
-    command.extend(["--tab-width", "2"])
-    command.extend(["--use-spaces"])
+    # brew install sqlfmt
+    # command = ["sqlfmt"]
+    # command.extend(["--print-width", "100"])
+    # command.extend(["--tab-width", "2"])
+    # command.extend(["--use-spaces"])
+
+    # brew install pgformatter
+    command = ["pg_format"]
+    command.extend(["--comma-break"])
+    command.extend(["--comma-end"])
+    command.extend(["--spaces=2"])
+    command.extend(["--wrap-limit=120"])
+    command.extend(["-"])
+
+    print("Formatting SQL:", ' '.join(command))
 
     result = subprocess.run(command, text=True, input=content, capture_output=True)
     result.check_returncode()
