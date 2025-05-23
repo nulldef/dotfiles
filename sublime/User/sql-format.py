5c408cba84fc05ee29a0d923f84cd12f33625fff
Aleksei Bespalov <aleksei.bespalov@smartly.io>
2025-05-23 16:03:16 +0200

    updates

--- sublime/User/sql-format.py
+++ sublime/User/sql-format.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 import subprocess
+import re
 
 class SqlFormatCommand(sublime_plugin.TextCommand):
   def run(self, edit):
@@ -9,22 +10,33 @@ class SqlFormatCommand(sublime_plugin.TextCommand):
     content = self.view.substr(region)
 
     # brew install sqlfmt
-    # command = ["sqlfmt"]
-    # command.extend(["--print-width", "100"])
-    # command.extend(["--tab-width", "2"])
-    # command.extend(["--use-spaces"])
+    command = ["sqlfmt"]
+    command.extend(["--print-width", "120"])
+    command.extend(["--tab-width", "2"])
+    command.extend(["--use-spaces"])
+    command.extend(["--no-simplify"])
 
     # brew install pgformatter
-    command = ["pg_format"]
-    command.extend(["--comma-break"])
-    command.extend(["--comma-end"])
-    command.extend(["--spaces=2"])
-    command.extend(["--wrap-limit=120"])
-    command.extend(["-"])
+    # command = ["pg_format"]
+    # command.extend(["--comma-break"])
+    # command.extend(["--comma-end"])
+    # command.extend(["--spaces=2"])
+    # command.extend(["--wrap-limit=120"])
+    # command.extend(["-"])
 
     print("Formatting SQL:", ' '.join(command))
 
     result = subprocess.run(command, text=True, input=content, capture_output=True)
-    result.check_returncode()
+    if result.returncode != 0:
+      raise Exception(result.stderr or result.stdout)
 
-    self.view.replace(edit, region, result.stdout)
+    formatted = result.stdout
+    formatted = re.sub(r',\)', ')', formatted, flags=re.IGNORECASE)
+    formatted = re.sub(r'::string', '::text', formatted, flags=re.IGNORECASE)
+    formatted = re.sub(r'::bytes', '::bytea', formatted, flags=re.IGNORECASE)
+    formatted = re.sub(r'::int8', '::int', formatted, flags=re.IGNORECASE)
+    formatted = re.sub(r'::float8', '::float', formatted, flags=re.IGNORECASE)
+    formatted = formatted.replace('e\'\\\\x', '\'\\x')
+    formatted = formatted.replace('current_timestamp()', 'current_timestamp')
+
+    self.view.replace(edit, region, formatted)
